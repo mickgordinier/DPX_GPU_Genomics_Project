94e40bdef0688646f501b2617df36295697be7d9
Mick Gordinier <mickg@gl-login1.arc-ts.umich.edu>
2025-04-09 19:27:15 -0400

    Added needed host code. Not tested.

--- cuda/LinearNeedlemanWunsch.cu
+++ cuda/LinearNeedlemanWunsch.cu
@@ -127,91 +127,117 @@ int main() {
 
     // TODO: NEED WAY TO RECEIVE USER INPUT
     // Need to be able to input reference and query strings
+    // TEMPORARY HARD CODED STRING AND WEIGHT VALUES
+    const string referenceString = "??????";
+    const string queryString = "??????";
+
+    const int matchWeight = 3;
+    const int mismatchWeight = -2;
+    const int gapWeight = -1;
+
 
     // Allocate device memory for matrices
-    int *deviceScoringMatrix, deviceBacktrackMatrix;
+    int *deviceScoringMatrix;
+    direction *deviceBacktrackMatrix;
     cudaMalloc(&deviceScoringMatrix, (referenceLength+1) * (queryLength+1) * sizeof(int));
-    cudaMalloc(&deviceBacktrackMatrix, (referenceLength+1) * (queryLength+1) * sizeof(int));
+    cudaMalloc(&deviceBacktrackMatrix, (referenceLength+1) * (queryLength+1) * sizeof(direction));
+
 
     // Need to launch kernel
-
-    // Need to perform bactracking
-
-
-
+    // Launching a kernel with 1 block with BLOCK_SIZE threads to populate scoring matrix
+    needleman_wunsch_forward_pass_kernel<<<1, BLOCK_SIZE>>>(
+        deviceScoringMatrix, deviceBacktrackMatrix,
+        queryString, referenceString, queryString.size(), referenceString.size(), 
+        matchWeight, mismatchWeight, gapWeight)
 
 
     // Allocate host memory for matrices
     // Allow for matrices to come from device -> host
+    // Free up device memory
     int *hostScoringMatrix = new int[(referenceLength+1) * (queryLength+1)];
     int *hostBacktrackMatrix = new int[(referenceLength+1) * (queryLength+1)];
 
     cudaMemcpy(hostScoringMatrix, deviceScoringMatrix, (referenceLength+1) * (queryLength+1) * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(hostBacktrackMatrix, deviceBacktrackMatrix, (referenceLength+1) * (queryLength+1) * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostBacktrackMatrix, deviceBacktrackMatrix, (referenceLength+1) * (queryLength+1) * sizeof(direction), cudaMemcpyDeviceToHost);
 
     cudaFree(deviceScoringMatrix);
     cudaFree(deviceBacktrackMatrix);
 
 
+    // Perform backtracking on host and print results
+    cout << "[Finding max cells...]\n";
 
+    // We need to find the max score of the entire matrix
+    // + note the cell(s) that have this value
 
+    int currentMemoRow = hostBacktrackMatrix.size()-1;
+    int currentMemoCol = hostBacktrackMatrix[0].size()-1;
 
+    std::string referenceSequence = "";
+    std::string pairRelation = "";
+    std::string querySequence = "";
 
+    while ((currentMemoRow != 0) && (currentMemoCol != 0)) {
+        
+        // Determine the current cell's predecessor
+        switch (hostBacktrackMatrix[currentMemoRow][currentMemoCol]) {
+            
+            case MATCH:
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = "*" + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                --currentMemoCol;
+                break;
+            // end if match
 
+            case MISMATCH: 
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = "|" + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                --currentMemoCol;
+                break;
+            // end if mismatch
+            
+            case QUERY_DELETION:
+                referenceSequence = "_" + referenceSequence;
+                pairRelation = " " + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                break;
+            // end if query deletion
+            
+            case QUERY_INSERTION:
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = " " + pairRelation;
+                querySequence = "_" + querySequence;
+                --currentMemoCol;
+                break;
+            // end if query insertion
+            
+            default:
+                exit(1);
+            // end if upper gap
 
+        } // end switch
+    } // end while
 
+    print_matrix();
 
-    cout << "[Initializing Matrix...]\n";
-    size_t num_rows = query_str.size();
-    size_t num_cols = reference_str.size();
+    cout << "[Needleman-Wunsch Score: " << hostScoringMatrix[hostScoringMatrix.size()-1][hostScoringMatrix[0].size()-1] << "]\n\n";
+    cout << "[Sequence Pairing(s)]\n";
+    cout << "\n====================\n";
 
-    // Initialize scoring memo
-    // Need the additional 0th row and column for initial gap penalty scores
-    vector<int> temp_memo;
-    temp_memo.resize(num_cols + 1, 0);
-    memo.resize(num_rows + 1, temp_memo);
-
-
+    cout << referenceSequence << std::endl;
+    cout << pairRelation << std::endl;
+    cout << querySequence << std::endl;
+    
+    cout << "\n====================\n";
+    cout << std::endl;
 
 }
 
-
-
-
-
-
-void init_matrix(){
-    cout << "[Initializing Matrix...]\n";
-    size_t num_rows = query_str.size();
-    size_t num_cols = reference_str.size();
-
-    // Initialize scoring memo
-    // Need the additional 0th row and column for initial gap penalty scores
-    vector<int> temp_memo;
-    temp_memo.resize(num_cols + 1, 0);
-    memo.resize(num_rows + 1, temp_memo);
-
-    // Initialize backtracking memo
-    // As we are doing global alignment we need to be able to backtrack from 0th row and column
-    vector<direction> temp_backtrack;
-    temp_backtrack.resize(num_cols + 1, NONE);
-    backtrack_memo.resize(num_rows + 1, temp_backtrack);
-
-    // If query needed to do initial DELETIONS before entering reference
-    // Can be thought of adding insertions to reference
-    // As this is linear gap penalty, will be doing row_idx * gap_weight
-    for (int row_idx = 1; row_idx < num_rows+1; ++row_idx) {
-        memo[row_idx][0] = row_idx * gap_weight;
-        backtrack_memo[row_idx][0] = QUERY_DELETION;
-    }
-
-    // If query needed to do initial INSERTIONS before entering reference
-    // As this is linear gap penalty, will be doing row_idx * gap_weight
-    for (int col_idx = 1; col_idx < num_cols+1; ++col_idx) {
-        memo[0][col_idx] = col_idx * gap_weight;
-        backtrack_memo[0][col_idx] = QUERY_INSERTION;
-    }
-}
 
 void LinearNeedlemanWunsch::print_matrix(){
     
@@ -257,159 +283,3 @@ void LinearNeedlemanWunsch::print_matrix(){
     cout << std::endl;
 
 }
-
-void LinearNeedlemanWunsch::score_matrix() {
-    cout << "[Scoring...]\n";
-
-    bool pred;
-
-    // Score the smith waterman matrix, keeping track of where a cell got its value
-    for(size_t row_idx = 1; row_idx < memo.size(); row_idx++) {
-        for(size_t col_idx = 1; col_idx < memo[0].size(); col_idx++) {
-            
-            int queryInsertionScore;
-            int queryDeletionScore;
-            int matchMismatchScore;
-            int largestScore;
-            
-            direction corner_direction = NONE;
-
-            // If match, add the match score from the corner
-            if (query_str[row_idx - 1] == reference_str[col_idx - 1]){
-                matchMismatchScore = memo[row_idx - 1][col_idx - 1] + match_weight;
-                corner_direction = MATCH;
-            }
-            // Otherwise, add the penalty score from the corner
-            else {
-                matchMismatchScore = memo[row_idx - 1][col_idx - 1] + mismatch_weight;
-                corner_direction = MISMATCH;
-            }
-            
-            // Calculate potential gap scores
-            queryDeletionScore = memo[row_idx - 1][col_idx] + gap_weight;
-            queryInsertionScore = memo[row_idx][col_idx - 1] + gap_weight;
-            
-            largestScore = FakeDPX::__vibmax_s32(queryDeletionScore, matchMismatchScore, &pred);
-            if (pred) corner_direction = QUERY_DELETION;
-            
-            largestScore = FakeDPX::__vibmax_s32(queryInsertionScore, largestScore, &pred);
-            if (pred) corner_direction = QUERY_INSERTION;
-
-            memo[row_idx][col_idx] = largestScore;
-            backtrack_memo[row_idx][col_idx] = corner_direction;
-
-        } // end for col_idx
-    } // end for row_idx
-
-    cout << std::endl;
-}
-
-void LinearNeedlemanWunsch::backtrack(){
-    cout << "[Finding max cells...]\n";
-
-    // We need to find the max score of the entire matrix
-    // + note the cell(s) that have this value
-
-    int currentMemoRow = memo.size()-1;
-    int currentMemoCol = memo[0].size()-1;
-
-    std::string referenceSequence = "";
-    std::string pairRelation = "";
-    std::string querySequence = "";
-
-    while ((currentMemoRow != 0) && (currentMemoCol != 0)) {
-        
-        // Determine the current cell's predecessor
-        switch (backtrack_memo[currentMemoRow][currentMemoCol]) {
-            
-            case MATCH:
-                referenceSequence = reference_str[currentMemoCol-1] + referenceSequence;
-                pairRelation = "*" + pairRelation;
-                querySequence = query_str[currentMemoRow-1] + querySequence;
-                --currentMemoRow;
-                --currentMemoCol;
-                break;
-            // end if match
-
-            case MISMATCH: 
-                referenceSequence = reference_str[currentMemoCol-1] + referenceSequence;
-                pairRelation = "|" + pairRelation;
-                querySequence = query_str[currentMemoRow-1] + querySequence;
-                --currentMemoRow;
-                --currentMemoCol;
-                break;
-            // end if mismatch
-            
-            case QUERY_DELETION:
-                referenceSequence = "_" + referenceSequence;
-                pairRelation = " " + pairRelation;
-                querySequence = query_str[currentMemoRow-1] + querySequence;
-                --currentMemoRow;
-                break;
-            // end if query deletion
-            
-            case QUERY_INSERTION:
-                referenceSequence = reference_str[currentMemoCol-1] + referenceSequence;
-                pairRelation = " " + pairRelation;
-                querySequence = "_" + querySequence;
-                --currentMemoCol;
-                break;
-            // end if query insertion
-            
-            default:
-                exit(1);
-            // end if upper gap
-
-        } // end switch
-    } // end while
-
-    print_matrix();
-
-    cout << "[Needleman-Wunsch Score: " << memo[memo.size()-1][memo[0].size()-1] << "]\n\n";
-    cout << "[Sequence Pairing(s)]\n";
-    cout << "\n====================\n";
-
-    cout << referenceSequence << std::endl;
-    cout << pairRelation << std::endl;
-    cout << querySequence << std::endl;
-    
-    cout << "\n====================\n";
-    cout << std::endl;
-}
-
-void LinearNeedlemanWunsch::align(){
-    init_matrix();
-    print_matrix();
-    score_matrix();
-    backtrack();
-}
-
-void LinearNeedlemanWunsch::print_results(){
-//     cout << "[Scored Matrix]\n";
-//     print_matrix();
-
-//     cout << "[# Highest Scoring Sequences: " << results.size() << " Score: " << max_score << "]\n\n";
-//     cout << "[Sequence Pairing(s)]\n";
-//     cout << "\n====================\n";
-
-//     for(size_t idx = 0; idx < results.size(); idx++){
-//         cout << "Sequence Pair: " << idx << "\n";
-//         backtrack_info backtrack_record = results[idx];
-//         for(size_t base = 0; base < backtrack_record.query_sequence.size(); base++){
-//             cout << backtrack_record.query_sequence[base];
-//         }
-//         cout << "\n";
-//         for(size_t base = 0; base < backtrack_record.pair_relation.size(); base++){
-//             cout << backtrack_record.pair_relation[base];
-//         }
-//         cout << "\n";
-//         for(size_t base = 0; base < backtrack_record.reference_sequence.size(); base++){
-//             cout << backtrack_record.reference_sequence[base];
-//         }
-//         cout << "\n====================\n";
-//     }
-
-//     cout << std::endl;
-}
-
-
