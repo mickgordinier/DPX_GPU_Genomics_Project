30ee094636f7f5a8638ad33199c6d467e2c66bf0
Christian David Reed <chrireed@gl-login3.arc-ts.umich.edu>
2025-04-11 13:54:56 -0400

    cleaning up stuff and adding scores as input args

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,61 +1,64 @@
-#include <iostream>
 #include <cstring>
 #include "parseInput.h"
 #include "LinearSmithWaterman.h"
 #include "LinearNeedlemanWunsch.h"
 
-using std::string;
-using std::vector;
-using std::ifstream;
-using std::ofstream;
-using std::endl;
-using std::getline;
 using std::strcmp;
-
-
 
 int main(int argc, char *argv[]){
 
     // Check that YOU use it correctly
     if (argc < 2) {
-		fprintf(stderr, "usage: main -pairs <InSeqFile>\n");
+		fprintf(stderr, "usage: main -pairs <InSeqFile> -match <matchWeight> -mismatch <mismatchWeight> -gap <gapWeight> \n");
 		exit(EXIT_FAILURE);
-	}
+    }
 	
     // Get filename from args
-    string pairFileName;
-	if(strcmp(argv[1], "-pairs") == 0) {
-		pairFileName = argv[2];
+    char *pairFileName;
+    int matchWeight     = 3;
+    int mismatchWeight  = -1;
+    int gapWeight       = -2;
+    if(strcmp(argv[1], "-pairs") == 0) {
+        pairFileName = argv[2];
+    }
+    if(argc > 3 && strcmp(argv[3], "-match") == 0) {
+        matchWeight = atoi(argv[4]);
+    }
+    if(argc > 5 && strcmp(argv[5], "-mismatch") == 0) {
+        mismatchWeight = atoi(argv[6]);
+    }
+    if(argc > 7 && strcmp(argv[7], "-gap") == 0) {
+        gapWeight = atoi(argv[8]);
     }
 
     // Parse input file
-    std::cout << "Parsing input file: " << pairFileName << std::endl;
+    printf("Parsing input file: %s\n", pairFileName);
     size_t numPairs;
     seqPair* sequenceIdxs;
     char* sequences;
-	numPairs = parseInput(pairFileName.c_str(), sequenceIdxs, sequences);
+    numPairs = parseInput(pairFileName, sequenceIdxs, sequences);
 
     // Print out the first byte of each section to make sure I'm not crazy
     // std::cout << "Printing sequence info" << std::endl;
     // printParsedFile(numPairs, sequenceIdxs, sequences);
 
     // Score some matrices babyyyyyyyyyyy
-    std::cout << "Pair # | Score\n";
+    printf("Pair # | Score\n");
     // LinearSmithWaterman LSW("GAGTACTCAACACCAACATTGATGGGCAATGGAAAATAGCCTTCGCCATCACACCATTAAGGGTGA", "GAATACTCAACAGCAACATCAACGGGCAGCAGAAAATAGGCTTTGCCATCACTGCCATTAAGGATGTGGG", 3, -1, -2);
-    for(int i = 0; i < numPairs/1000; i++){
-        std::cout << i << " | ";
-        LinearSmithWaterman LSW(&sequences[sequenceIdxs[i].referenceIdx], &sequences[sequenceIdxs[i].queryIdx], 3, -1, -2);
-        LSW.align();
-    }
+    // for(int i = 0; i < numPairs/1000; i++){
+    //     printf("%d | ", i);
+    //     LinearSmithWaterman LSW(&sequences[sequenceIdxs[i].referenceIdx], &sequences[sequenceIdxs[i].queryIdx], matchWeight, mismatchWeight, gapWeight);
+    //     LSW.align();
+    // }
 
     // LinearNeedlemanWunsch LNW("GAGTACTCAACACCAACATTGATGGGCAATGGAAAATAGCCTTCGCCATCACACCATTAAGGGTGA", "GAATACTCAACAGCAACATCAACGGGCAGCAGAAAATAGGCTTTGCCATCACTGCCATTAAGGATGTGGG", 3, -1, -2);
     for(int i = 0; i < numPairs/1000; i++){
-        std::cout << i << " | ";
-        LinearNeedlemanWunsch LNW(&sequences[sequenceIdxs[i].referenceIdx], &sequences[sequenceIdxs[i].queryIdx], 3, -1, -2);
+        printf("%d |", i);
+        LinearNeedlemanWunsch LNW(&sequences[sequenceIdxs[i].referenceIdx], &sequences[sequenceIdxs[i].queryIdx], matchWeight, mismatchWeight, gapWeight);
         LNW.align();
     }
 
     // Free data arrays
-    std::cout << "Cleanup data arrays" << std::endl;
+    printf("Cleaning up\n");
     cleanupParsedFile(sequenceIdxs, sequences);
 }
