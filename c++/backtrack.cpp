e3bbd7bbe8e304e9b1935facd20a859a335f14e9
Christian David Reed <chrireed@gl-login1.arc-ts.umich.edu>
2025-04-16 15:24:56 -0400

    added profiling to LNW4

--- c++/backtrack.cpp
+++ c++/backtrack.cpp
@@ -80,6 +80,71 @@ void backtrackNW(const directionMain *backtrackMemo, const char *referenceString
     
 }
 
+void backtrackMultiNW(const directionMain *backtrackMemo, const char *referenceString, const int referenceLength, const char *queryString, const int queryLength, const int pairNum, const int score){
+    int numRows = queryLength + 1;
+    int numCols = referenceLength + 1;
+    int currentMemoRow = queryLength;
+    int currentMemoCol = referenceLength;
+
+    std::string referenceSequence = "";
+    std::string pairRelation = "";
+    std::string querySequence = "";
+
+    while ((currentMemoRow != 0) || (currentMemoCol != 0)) {
+        
+        // Determine the current cell's predecessor
+        switch (backtrackMemo[(currentMemoRow * numCols) + currentMemoCol]) {
+            
+            case MATCH:
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = "*" + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                --currentMemoCol;
+                break;
+            // end if match
+
+            case MISMATCH: 
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = "|" + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                --currentMemoCol;
+                break;
+            // end if mismatch
+            
+            case QUERY_DELETION:
+                referenceSequence = "_" + referenceSequence;
+                pairRelation = " " + pairRelation;
+                querySequence = queryString[currentMemoRow-1] + querySequence;
+                --currentMemoRow;
+                break;
+            // end if query deletion
+            
+            case QUERY_INSERTION:
+                referenceSequence = referenceString[currentMemoCol-1] + referenceSequence;
+                pairRelation = " " + pairRelation;
+                querySequence = "_" + querySequence;
+                --currentMemoCol;
+                break;
+            // end if query insertion
+            
+            default:
+                exit(1);
+            // end if upper gap
+
+        } // end switch
+    } // end while
+
+    printLock();
+    printf("%d | %d\n", pairNum, score);
+    printf("%s\n", referenceSequence.c_str());
+    printf("%s\n", pairRelation.c_str());
+    printf("%s\n", querySequence.c_str());
+    printUnlock();
+    
+}
+
 void backtrackANW(
     const directionMain *scoringBacktrack, 
     const directionIndel *queryInsertionBacktrack,
